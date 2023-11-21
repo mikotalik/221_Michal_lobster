3f591780a5c0c5d8a81845bafa2cde9ad965d6f4
mikotalik <mikotalik@seznam.cz>
2023-11-21 17:13:18 +0100

    Trip added

--- public/main.js
+++ public/main.js
@@ -1,20 +1,12 @@
 import * as THREE from 'three'
-import { GLTFLoader } from "./jsm/loaders/GLTFLoader.js"
-import { AmbientLight, DirectionalLight, MeshNormalMaterial, Object3D, Vector3 } from 'three'
-import { VideoPlane } from './videoplane.js';
-import { VideoSphere } from './videosphere.js';
+import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 import { animateAnything } from './animator.js';
-import { Card } from './card.js';
-import { TEXTURES } from './constants.js';
-import { Button } from './button.js';
-
-//import { default as config } from './data/config.json' assert { type: 'json' };
 
 console.clear();
 
-let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+let renderer = new CSS3DRenderer()
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0, 0, 0)
 
 let frame = 0
@@ -23,73 +15,35 @@ let mouse = new THREE.Vector2()
 let pmouse = new THREE.Vector2()
 let mouseDown = false
 let mouseDownTime = 0
-const raycaster = new THREE.Raycaster()
-
-let fullscreenVideo
 
 console.log("Starting app load...")
 setupRendering()
 
 function setupRendering() {
     renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.setPixelRatio(2)
-
-    renderer.shadowMap.enabled = true;
-    renderer.shadowCameraNear = 0.5;
-    renderer.shadowCameraFar = 500;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
     document.body.appendChild(renderer.domElement)
 
     setup()
+    animate()
 }
 
 function setup() {
-    scene.background = new THREE.Color(0.1, 0.1, 0.1)
+    document.body.style.backgroundColor="#001a79"
     scene.add(camera)
 
-    const light_ambient = new AmbientLight(0xffffff);
-    light_ambient.intensity = 0.5;
-    scene.add(light_ambient);
+    const video_preparation = new CSS3DObject(document.getElementById("section-preparation"))
+    video_preparation.rotateY((Math.PI*2)*0.0)
+    video_preparation.translateZ(-750)
+    scene.add(video_preparation)
 
-    const vid = new VideoPlane("video", false, "LobsterImmersiveExperienceDevelopment/FinalLobsterImmersiveExperience/1%20-%20BoatTrip/1.%20Preparation&SettingPots/PreparationSettingPots_OPT.mp4")
-    vid.position.z = -3
-    scene.add(vid)
-
-    const vid2 = new VideoPlane("video", true, "LobsterImmersiveExperienceDevelopment%5CFinalLobsterImmersiveExperience%5C1%20-%20BoatTrip%5C2.%20CatchingLobster%5CCatchingLobster_OPT.mp4")
-    vid2.position.z = -3
-    vid2.position.x = 2
-    scene.add(vid2)
-
-    const vid3 = new VideoPlane("video", false, "LobsterImmersiveExperienceDevelopment%5CFinalLobsterImmersiveExperience%5C1%20-%20BoatTrip%5C3.%20LobsterFishing%5CLobsterFishing_OPT.mp4")
-    vid3.position.z = -3
-    vid3.position.x = -2
-    scene.add(vid3)
-
-    const sphere = new VideoSphere("sphere", true, "LobsterImmersiveExperienceDevelopment/FinalLobsterImmersiveExperience/1%20-%20BoatTrip/1.%20Preparation&SettingPots/PreparationSettingPots_OPT.mp4")
-    scene.add(sphere)
-    sphere.play(true)
-
-    const card = new Card("card", "exports/card_front.png", "exports/card_back.png")
-    card.translateZ(-3)
-    card.translateY(1.3)
-    scene.add(card)
-
-    const button = new Button("button", "exports/button.png")
-    button.translateZ(-3)
-    button.translateY(-1)
-    scene.add(button)
-
-    renderer.setAnimationLoop(draw);
-
-    console.log("App loaded.")
-
-    
-    //animateAnything(vid, "translateY", 1000, [vid.position.y], [1])
-    //animateAnything(document, "title", 2000, [0], [100])
+    const video_preparation2 = new CSS3DObject(document.getElementById("section-sustainability"))
+    video_preparation2.rotateY((Math.PI*2)*0.2)
+    video_preparation2.translateZ(-750)
+    scene.add(video_preparation2)
 }
 
-function draw() {
+function animate() {
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
     frame++
 }
@@ -106,32 +60,13 @@ function onMouseMove(event) {
     if (mouseDown) {
         camera.rotateY(mouse.x - pmouse.x)
     }
-
-    //console.log(mouse.distanceTo(pmouse))
-
-
 }
 
 function onMouseDown(event) {
     mouseDown = true
 
     if (event.timeStamp - mouseDownTime < 200) {
-        raycaster.setFromCamera(mouse, camera)
-        const intersection = raycaster.intersectObjects(scene.children);
-        if (intersection.length > 0) {
-            const object = intersection[0].object;
-
-            if (object.name == "video") {
-                if (fullscreenVideo) {
-                    fullscreenVideo.fullscreen(false)
-                    fullscreenVideo = null
-
-                } else {
-                    fullscreenVideo = object
-                    fullscreenVideo.fullscreen(true)
-                }
-            }
-        }
+        
     }
 
     mouseDownTime = event.timeStamp
@@ -140,42 +75,11 @@ function onMouseDown(event) {
 function onMouseUp(event) {
     mouseDown = false
 
-    raycaster.setFromCamera(mouse, camera)
-    const intersection = raycaster.intersectObjects(scene.children);
-
-    if (intersection.length > 0) {
-        let object = intersection[0].object;
 
         if (event.timeStamp - mouseDownTime < 90) {
 
-            if (object.name == "video") {
-                if (object.playing) {
-                    object.play(false)
-                } else {
-                    object.play(true)
-                }
-            }
-            if (object.name == "card") {
-                object = object.parent
-                if(object.frontFacing){
-                    animateAnything(object, "rotateX", 300,0, [0], [Math.PI])
-                    object.frontFacing = false
-                }else{
-                    animateAnything(object, "rotateX", 300,0, [0], [Math.PI])
-                    object.frontFacing = true
-                }
-            }
-            if (object.name == "button") {
-                animateAnything(object, "translateZ", 65,0, [0], [-0.1])
-                animateAnything(object, "translateZ", 65,75, [0], [0.1])
-                object.pressed = true
-
-                //animateAnything(document, "title", 1000, 0, [0], [100])
-                //animateAnything(console, "log", 1000, 0, [0], [1])
-            }
-
         }
-    }
+    
 }
 
 function onWindowResize() {
@@ -189,3 +93,4 @@ window.addEventListener("resize", onWindowResize)
 window.addEventListener('mousemove', onMouseMove);
 window.addEventListener("mousedown", onMouseDown)
 window.addEventListener("mouseup", onMouseUp)
+
